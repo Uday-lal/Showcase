9067ae60656404229b53afa877a239a294af4a46
Uday-lal <udaylal014@gmail.com>
2021-10-04 12:33:47 +0530

    Complate the header of the index

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from .api import Api
+import random
 
 
 views = Blueprint("views", __name__)
@@ -8,5 +9,6 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def index():
     unsplash_api = Api()
-    print(unsplash_api.get_images())
-    return render_template("index.html")
+    images = unsplash_api.get_images()
+    art_image = random.choice(unsplash_api.query("art", 1)["results"])
+    return render_template("index.html", art_image=art_image["urls"]["full"], images=images)
