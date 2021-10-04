e908be177da7b20d8f46ed41da8b7cd5a4fcbadb
Uday-lal <udaylal014@gmail.com>
2021-10-04 23:20:27 +0530

    complete the result page

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from .api import Api
 import random
 
@@ -6,12 +6,33 @@ import random
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        query = request.form.get("search")
+        return redirect(url_for('views.result', query=query))
     unsplash_api = Api()
     images = format_api(unsplash_api.get_images())
     art_image = random.choice(unsplash_api.query("art", 1)["results"])
-    return render_template("index.html", art_image=art_image["urls"]["full"], images=images)
+    return render_template(
+        "index.html",
+        art_image=art_image["urls"]["full"],
+        images=images
+    )
+
+
+@views.route("/result/<query>")
+def result(query):
+    unsplash_api = Api()
+    page_on = random.randint(1, 5)
+    images = format_api(unsplash_api.query(
+        query=query, page_on=page_on)["results"])
+    art_image = random.choice(unsplash_api.query("art", 1)["results"])
+    return render_template(
+        "result.html",
+        images=images,
+        art_image=art_image["urls"]["full"]
+    )
 
 
 def format_api(api: list):
