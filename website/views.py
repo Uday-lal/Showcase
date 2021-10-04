95a804a177107678af9a064fbe7c0b2d7ff02630
Uday-lal <udaylal014@gmail.com>
2021-10-04 18:49:54 +0530

    Complete the index page

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from .api import Api
 import random
+import pprint
 
 
 views = Blueprint("views", __name__)
@@ -9,6 +10,22 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def index():
     unsplash_api = Api()
-    images = unsplash_api.get_images()
+    images = format_api(unsplash_api.get_images())
     art_image = random.choice(unsplash_api.query("art", 1)["results"])
     return render_template("index.html", art_image=art_image["urls"]["full"], images=images)
+
+
+def format_api(api: list):
+    result = []
+    for i in range(3):
+        cols = []
+        j = i
+        while True:
+            try:
+                image = api[j]
+                cols.append(image)
+                j += 3
+            except IndexError:
+                break
+        result.append(cols)
+    return result
